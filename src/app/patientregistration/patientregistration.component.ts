bf9be1ca21bd88775839caf13661872ce8e5b2b4
Krishna <krishs09@yahoo.in>
2023-04-06 13:50:00 +0530

    updated commit

--- src/app/patientregistration/patientregistration.component.ts
+++ src/app/patientregistration/patientregistration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/model/Hero';
+import { UserPatient } from 'src/model/UserPatient';
+import { UserPatientCopy } from 'src/model/UserPatientCopy';
 
 @Component({
   selector: 'app-patientregistration',
@@ -7,11 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientregistrationComponent implements OnInit {
 
-  firstname:any;
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  gender = ['Male', 'Female',
+            'Others'];
+
+  model = new UserPatientCopy('bvcb', 'DIQ', this.gender[0], 'Chuck Overstreet','sada','dsad','dsa');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; 
+    console.log("Submitted: "+JSON.stringify(this.model));
+  }
+
+  newHero() {
+    this.model = new UserPatientCopy('', '', '', '', '', '', '');
   }
 
 }
---
+++ src/model/UserPatientCopy.ts
@@ -0,0 +1,18 @@
+export class UserPatientCopy{
+    public firstname:string="";
+    public lastname:string="";
+    public email:string="";
+    public dateOfBirth:string="";
+    public mobile:any;
+    public gender:any;
+    public user_id:any;
+    constructor(
+        firstname: string,
+        lastname: string,
+        email: string,
+       dateOfBirth: string,
+       mobile: any,
+       gender: any,
+        user_id: any
+      ) {  }
+}
